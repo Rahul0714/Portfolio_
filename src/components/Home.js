74704869a811f96dd43dca17abb0922b8a9f9ac1
Rahul Mujumdar <rahulmujumdar2000@gmail.com>
2023-08-28 21:49:58 +0530

    done with windows

--- src/components/Home.js
+++ src/components/Home.js
@@ -9,6 +9,7 @@ import LinkedinIcon from "./LinkedinIcon";
 import FullscreenIcon from "./FullscreenIcon";
 import { useSelector } from "react-redux";
 import useResize from "../utils/useResize";
+import PopUp from "./PopUp";
 
 const Home = () => {
   const [isClosed, setIsClosed] = useState(true);
@@ -22,33 +23,15 @@ const Home = () => {
   }, [isApple, windowSize]);
 
   return (
-    <div className="flex flex-col max-h-screen h-auto">
-      <div
-        className={`${
-          !isClosed ? "hidden" : "z-20"
-        } absolute left-[40%] text-white h-fit w-[1/2] p-2 rounded-xl backdrop-blur-lg border-[1px] border-blue-950 shadow-sm text-center`}
-      >
-        <div className="flex justify-center items-end gap-4 text-sm">
-          <div className="flex flex-col gap-1 items-center justify-center ">
-            <div className="mt-2 flex gap-2 items-center">
-              <IoBulbOutline /> Did you know?
-            </div>
-            <div>This portfolio will also look great on mobile</div>
-          </div>
-          <div
-            className="text-lg cursor-pointer p-1  w-10 hover:text-blue-700"
-            onClick={() => setIsClosed(false)}
-          >
-            OK
-          </div>
-        </div>
-      </div>
+    <div className="flex flex-col max-h-screen h-auto ">
+      <PopUp isClosed={isClosed} setIsClosed={setIsClosed} />
+
       <img
         className=" object-cover fixed w-full h-screen overflow-hidden -z-10"
         src={windows}
         alt="Windows"
       />
-      <div className="z-10 flex flex-row sm:flex-col items-start box-border m-4 gap-4 flex-wrap">
+      <div className=" flex flex-row sm:flex-col items-start box-border m-4 gap-4 flex-wrap">
         <ProjectIcon
           isShowProjectPressed={isShowProjectPressed}
           setIsShowProjectPressed={setIsShowProjectPressed}
@@ -57,8 +40,8 @@ const Home = () => {
         <GithubIcon />
         <LinkedinIcon />
         <FullscreenIcon />
-        <BottomNavbar />
       </div>
+      <BottomNavbar />
     </div>
   );
 };
---
+++ src/components/PopUp.js
@@ -0,0 +1,34 @@
+import React from "react";
+import useResize from "../utils/useResize";
+import { IoBulbOutline } from "react-icons/io5";
+
+const PopUp = ({ isClosed, setIsClosed }) => {
+  const windowSize = useResize();
+  return (
+    <div
+      className={`${
+        !isClosed ? "hidden" : "z-20"
+      } mx-auto text-white h-fit w-fit p-2 rounded-xl backdrop-blur-lg border-[1px] border-blue-950 shadow-sm text-center z-30 absolute left-[30%] right-[30%]`}
+    >
+      <div className="flex justify-center items-end gap-4 text-sm">
+        <div className="flex flex-col gap-1 items-center justify-center ">
+          <div className="mt-2 flex gap-2 items-center">
+            <IoBulbOutline /> Did you know?
+          </div>
+          <div>
+            This portfolio will also look great on
+            {windowSize ? " Web" : " Mobile"}
+          </div>
+        </div>
+        <div
+          className="text-lg cursor-pointer p-1 my-auto  w-10 hover:text-blue-700"
+          onClick={() => setIsClosed(false)}
+        >
+          OK
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PopUp;
